80ed2b402533e18ae34291d75331b27b7151b507
sami <raihannsamii11@gmail.com>
2025-07-09 08:29:27 +0700

    penambahan js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Halo, Git!')
+document.getElementById('search-input').addEventListener('keyup', function () {
+    const filter = this.value.toLowerCase();
+    const links = document.querySelectorAll('.link-container a');
+    
+    links.forEach(link => {
+        const text = link.textContent.toLowerCase();
+        if (text.includes(filter)) {
+            link.style.display = 'inline-block';
+        } else {
+            link.style.display = 'none';
+        }
+    });
+});
